dae9c103634f29d958f1b3d88943c2c2aab3df4b
Theo Everaere <72456593+Theo-Everaere@users.noreply.github.com>
2025-01-09 19:52:45 +0100

    fixed home page on mobile

--- src/Components/home/ScrollDown.js
+++ src/Components/home/ScrollDown.js
@@ -1,7 +1,7 @@
 const ScrollDown = () => {
   return (
     <div className="home__scroll">
-      <a href="#about">
+      <a href="#about" className="home__scroll-button button--flex">
         <svg
           width="32px"
           height="32px"
