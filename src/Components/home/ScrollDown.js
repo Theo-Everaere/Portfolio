8864b81786389f196ad947db75ebc3c269b7fc1d
Theo Everaere <72456593+Theo-Everaere@users.noreply.github.com>
2025-01-09 19:24:22 +0100

    fixed scroll down issues

--- src/Components/home/ScrollDown.js
+++ src/Components/home/ScrollDown.js
@@ -1,7 +1,7 @@
 const ScrollDown = () => {
   return (
     <div className="home__scroll">
-      <a href="#about" className="home__scroll-button button--flex">
+      <a href="#about">
         <svg
           width="32px"
           height="32px"
